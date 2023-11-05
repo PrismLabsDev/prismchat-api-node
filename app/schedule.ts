b293a4e1d587d960f680fbd144fff72e6c73b70b
Jack Woodrow <jackwoodrow99@gmail.com>
2023-11-05 18:30:35 -0500

    Remove commented schedule

--- app/schedule.ts
+++ app/schedule.ts
@@ -7,12 +7,6 @@ import Message from "./models/Message";
 import PushSubscription from "./models/PushSubscription";
 
 const run = () => {
-  // Test job every second
-  // schedule.scheduleJob('* * * * * *', async () => {
-  //   const backTime = new Date(dayjs().valueOf());
-  //   console.log(backTime);
-  // });
-
   // Run every hour
   schedule.scheduleJob('0 * * * *', async () => {
     const backTime = new Date(dayjs().subtract(30, 'days').valueOf());
